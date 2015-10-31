eddea0718bfc10372687cb83d29d47e00ef72824
masumnicht <masumnicht@gmail.com>
2015-10-30 20:24:10 -0700

    Sorta implemented get and post for controller and model.  The sql is probably working.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,90 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      if(req.url === '/' ||  req.url.slice(0, 7) === '../client') {
+        loadFile(req, res);
+      } else {
+        models.messages.get(function(results){
+          res.send(results)
+        });
+      }
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      models.messages.post(req.body, function (results) {
+        res.send(results);
+      })
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get(function(results){
+        res.send(results)
+      });
+    },
+    post: function (req, res) {
+      console.log(req.body);
+      models.users.post(req.body, function (results) {
+        res.send(results);
+      })
+    }
   }
 };
+
+
+
+
+
+
+
+
+
+var loadFile = function (request, response) {
+  var type = '';
+  var extension = request.url.slice(-3)
+  var path = (request.url === '/') ? '/refactor.html' : request.url;
+
+  if(extension === '.js') {
+    type = 'text/javascript'
+  } else if (extension === 'css') {
+    type = 'text/css';
+  } else if (extension === 'gif') {
+    type = 'image/gif';
+  } else {
+    type = 'text/html';
+  }
+
+  var file_stream = fs.createReadStream('.' + path);
+
+  collectData(file_stream, function(rawData) {
+    sendResponse(response, rawData, 200, type);
+  });
+}
+
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+var sendResponse = function (response, data, statusCode, contentType) {
+  headers['Content-Type'] = contentType;
+  response.writeHead(statusCode, headers);
+  response.end(data);
+}
+
+var collectData = function (request, callback) {
+  var data = '';
+  request.on('data', function (json) {
+    data += json;
+  })
+  request.on('end', function () {
+    callback(data);
+  })
+}
+
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,55 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      var DB = db();
+      DB.connect();
+      DB.query('SELECT * from messages', function (err, messages) {
+        if(err) {
+          throw err
+        } else {
+          callback(messages)
+        }
+      })
+      // DB.end();
+    }, // a function which produces all the messages
+    post: function (messageData, callback) {
+      var DB = db();
+      var user = messageData.userName;
+      userExists(user)
+      DB.connect();
+
+      DB.query('INSERT ')
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      var DB = db();
+      DB.connect();
+      DB.query('SELECT * from users', function (err, users) {
+        if(err) throw err;
+        else {
+          callback(users);
+        }
+      })
+    },
+    post: function (userData, callback) {
+
+    }
   }
 };
 
+// var userExists = function (userName, callback) {
+//   var DB = db();
+//   DB.connect();
+//   DB.query('SELECT userId, userName')
+
+// }
+
+/*
+
+
+
+*/
